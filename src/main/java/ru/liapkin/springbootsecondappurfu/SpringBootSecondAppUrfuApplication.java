9cf7e957c926af73b8e945838446f766a396532c
lyapkin <lyapkin992@gmail.com>
2023-10-07 13:02:38 +0500

    init

---
+++ src/main/java/ru/liapkin/springbootsecondappurfu/SpringBootSecondAppUrfuApplication.java
@@ -0,0 +1,13 @@
+package ru.liapkin.springbootsecondappurfu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecondAppUrfuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecondAppUrfuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/liapkin/springbootsecondappurfu/SpringBootSecondAppUrfuApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.liapkin.springbootsecondappurfu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecondAppUrfuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
